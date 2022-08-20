814e5f60a02619b655be374a13325a0032853756
Miguel Monroy Lopez <miguel56mrypz@gmail.com>
2022-08-20 12:42:53 -0500

    Miguel Monroy Lopez

--- src/PROYECTO/HolaMundo.java
+++ src/PROYECTO/HolaMundo.java
@@ -3,5 +3,6 @@ package PROYECTO;
 public class HolaMundo {
     public static void main(String[] args){
         System.out.println("Miguel Monroy López");
+        System.out.println("Hello world");
     }
 }
