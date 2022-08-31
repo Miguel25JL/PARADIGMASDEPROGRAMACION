d4ff710c7e0b39e1fd79673d77a29b0ab2eaaab4
Miguel Monroy Lopez <miguel56mrypz@gmail.com>
2022-08-31 15:12:28 -0500

    Miguel Monroy Lopez

--- src/EJERCICIOCLASE/Persona.java
+++ src/EJERCICIOCLASE/Persona.java
@@ -4,19 +4,23 @@ public class Persona {
     private String nombre;
     private int edad;
     private String sexo;
-    private String direccion;
+    private  String direccion;
 
-    public Persona(String nombre, int edad, String sexu, String direccion){
-        this.nombre=nombre;
-        this.edad=edad;
-        this.sexo=sexu;
-        this.direccion=direccion;
+    public Persona(String nombre, int edad, String sexo, String direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public void mostrarInfo(){
-        System.out.println("Nombre"+ nombre);
-        System.out.println("Edad"+ edad);
-        System.out.println("Sexo" + sexo);
-        System.out.println("Direccion"+ direccion);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Edad: "+edad);
+        System.out.println("Sexo: "+sexo);
+        System.out.println("Direccion: "+direccion);
     }
 }
--- src/EJERCICIOCLASE/Principal.java
+++ src/EJERCICIOCLASE/Principal.java
@@ -2,11 +2,13 @@ package EJERCICIOCLASE;
 
 public class Principal {
     public static void main(String[] args) {
-        //Persona P1 = new Persona();
-        //Estudiante e1 = new Estudiante();
+        Persona p1=new Persona("Maria",12,"F","jocotitlan");
+        Estudiante e1=new Estudiante("juan",17,"M","atlacomulco","0123","paradigmas","B+");
 
-        //mm
-        //e1.mostrarInfo();
-        //P1.mostrar();
+        e1.mostrarInfo();
+        p1.mostrarInfo();
+
+
+
     }
 }
